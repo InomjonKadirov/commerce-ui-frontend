62b55d3087e4b569fb62ed1afe0ae619c7a00752
inomjonqodirov@gmail.com <Mirzobek50$>
2020-05-03 03:58:24 +0500

    1st commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,38 @@
+import { Routes } from '@angular/router';
+
+import { MainComponent } from './main/main.component';
+
+export const rootRouterConfig: Routes = [
+  {
+    path: '',
+    redirectTo: 'home/one',
+    pathMatch: 'full'
+  },
+  // {
+  //   path: 'demo',
+  //   component: DemoComponent
+  // },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)
+      },
+      {
+        path: 'pages',
+        loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+      },
+      {
+        path: 'blog',
+        loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home/one'
+  }
+];
+
