86ccc196cd6b4c7ea94cfaf6c4ee8055587c297f
inomjonqodirov@gmail.com <Mirzobek50$>
2020-05-04 00:44:26 +0500

    everything works fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 
 import * as $ from 'jquery';
+import {Ng2SearchPipeModule} from "ng2-search-filter";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -34,6 +35,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpModule,
     BrowserAnimationsModule,
     ShopModule,
+    Ng2SearchPipeModule,
     SharedModule,
     HttpClientModule,
     NgbModule,
